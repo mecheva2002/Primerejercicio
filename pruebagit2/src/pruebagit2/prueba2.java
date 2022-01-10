83e055b6a6c24366520bb2be4e4230db593ae847
miguel <you@example.com>
2022-01-10 20:00:22 +0100

    primer commit

--- pruebagit2/src/pruebagit2/prueba2.java
+++ pruebagit2/src/pruebagit2/prueba2.java
@@ -5,6 +5,7 @@ public class prueba2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("holaasdsxfghda");
-	}
+	
+	System.out.println("10/01/2022");
 
-}
+}}
